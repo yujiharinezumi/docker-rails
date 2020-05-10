edfc0d7d5058e11dc705af6603e3ac5311e9edb9
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-05-10 21:27:10 +0900

    created comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  comment    :text(65535)      not null
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
+class Comment < ApplicationRecord
+  belongs_to :board
+end
