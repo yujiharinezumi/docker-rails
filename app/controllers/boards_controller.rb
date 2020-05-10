358b2f7a2b5d33df987c2b8d85cc7bc4d6a0cc2f
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-05-10 13:23:52 +0900

    added partial files to refactoring

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,16 +9,27 @@ class BoardsController < ApplicationController
 
   def create
     Board.create(board_params)
-    
+
   end
 
   def show
     @board = Board.find(params[:id])
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+     board = Board.find(params[:id])
+     board.update(board_params)
+     
+     redirect_to board
+  end
+
   private
 
-  def board_params  
+  def board_params
     params.require(:board).permit(:name, :title, :body)
   end
 
