7f7391f4f8b7eb0208bf709a7fe6d5791147923e
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-05-10 10:00:24 +0900

    added create function & setting time zone

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,23 @@
 class BoardsController < ApplicationController
   def index
-
+    @boards = Board.all
   end
 
-  def new 
+  def new
     @board = Board.new
+    # binding.pry
+  end
+
+  def create
+    Board.create(board_params)
+    #binding.pry
+    
+  end
+
+  private
+
+  def board_params  
+    params.require(:board).permit(:name, :title, :body)
   end
 
 end
