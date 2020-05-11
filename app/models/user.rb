cd7973b27592a137487473df999ec9349b7cf9fc
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-05-11 13:48:28 +0900

    added user & session controller

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+end
