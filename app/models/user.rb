8d782e033c67f2b2db03994b6f80f8d10949e108
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2020-05-11 14:00:52 +0900

    added users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
+  
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: {maximum: 16 },
+    format: {
+　　  with:  /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+   validates :password,
+    length: { minimum: 8 }
 end
